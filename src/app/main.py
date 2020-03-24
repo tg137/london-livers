3b892734bd2b132a4004807a1f0556376e67b0a8
Tom Gowland <tkg137@gmail.com>
2020-03-24 18:13:47 +0000

    refactor: change main.py to use new suffix configs

--- src/app/main.py
+++ src/app/main.py
@@ -2,7 +2,7 @@ import urllib
 
 from fastapi import FastAPI, HTTPException
 import requests
-from app.utils.config import BASE_URL
+from app.utils.config import BASE_URL, LONDON_USERS_URL_SUFFIX, USERS_URL_SUFFIX
 
 app = FastAPI()
 
@@ -10,11 +10,11 @@ app = FastAPI()
 @app.get("/users-in-london")
 def get_users_in_london():
     # Get the users who live in London
-    city_url = urllib.parse.urljoin(BASE_URL, "city/London/users")
+    city_url = urllib.parse.urljoin(BASE_URL, LONDON_USERS_URL_SUFFIX)
     city_response = requests.get(city_url)
 
     # Get the full list of users
-    users_url = urllib.parse.urljoin(BASE_URL, "users")
+    users_url = urllib.parse.urljoin(BASE_URL, USERS_URL_SUFFIX)
     users_response = requests.get(users_url)
 
     response = {"living_in_london": city_response.json(), "within_50_miles": users_response.json()}
