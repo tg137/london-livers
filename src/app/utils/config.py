cf98f937f07b82c2d134acd9517a119f52e49388
Tom Gowland <tkg137@gmail.com>
2020-03-24 17:47:10 +0000

    chore: add URL suffixes to the config

--- src/app/utils/config.py
+++ src/app/utils/config.py
@@ -4,6 +4,9 @@ config = configparser.ConfigParser()
 config.read("config.ini")
 
 BASE_URL = config["DEFAULT"]["BaseURL"]
+USERS_URL_SUFFIX = config["DEFAULT"]["UsersUrlSuffix"]
+LONDON_USERS_URL_SUFFIX = config["DEFAULT"]["UsersInLondonUrlSuffix"]
+
 EARTH_RADIUS = config.getfloat("DEFAULT", "EarthRadius")
 KILOMETRES_TO_MILES_CONVERSION_FACTOR = config.getfloat("DEFAULT", "KilometresToMilesFactor")
 LONDON_LATITUDE = config.getfloat("DEFAULT", "LondonLatitude")
