c8c78d94fd769966b8e4a66f25e16fb40ef06622
Tom Gowland <tkg137@gmail.com>
2020-03-24 17:26:58 +0000

    chore: add config for London's latitude and longitude

--- src/app/utils/config.py
+++ src/app/utils/config.py
@@ -6,3 +6,5 @@ config.read("config.ini")
 BASE_URL = config["DEFAULT"]["BaseURL"]
 EARTH_RADIUS = config.getfloat("DEFAULT", "EarthRadius")
 KILOMETRES_TO_MILES_CONVERSION_FACTOR = config.getfloat("DEFAULT", "KilometresToMilesFactor")
+LONDON_LATITUDE = config.getfloat("DEFAULT", "LondonLatitude")
+LONDON_LONGITUDE = config.getfloat("DEFAULT", "LondonLongitude")
