b2934fb95d93ef590aa967fd144654b477719bbd
Tom Gowland <tkg137@gmail.com>
2020-03-24 18:24:53 +0000

    docs: adding docstring to config.py

--- src/app/utils/config.py
+++ src/app/utils/config.py
@@ -1,3 +1,8 @@
+"""
+The config module reads the config parameters in from the config.ini file
+and exposes them as variables that can be imported into other modules.
+"""
+
 import configparser
 
 config = configparser.ConfigParser()
